9b288954804a841931ff4d7f2118fd5b46637360
Lamparter <71598437+Lamparter@users.noreply.github.com>
2024-12-14 12:08:47 +0000

    Add remarks to `ITranslatable`

--- src/Riverside.Markup.InteropServices/ITranslatable.cs
+++ src/Riverside.Markup.InteropServices/ITranslatable.cs
@@ -5,6 +5,9 @@ namespace Riverside.Markup.InteropServices
     /// <summary>
     /// Interface for documents that can be converted to Rosetta format.
     /// </summary>
+    /// <remarks>
+    /// The main implementations for this interface are in the Riverside.Markup.InteropServices namespace, however the interface is marked as public to enable consumers to build their own Rosetta pipes.
+    /// </remarks>
     public interface ITranslatable
     {
         /// <summary>
