525af4d229b4035d28b6f35adec0956806df08be
Lamparter <71598437+Lamparter@users.noreply.github.com>
2024-12-14 11:38:52 +0000

    Add translatable document types enum

---
+++ src/Riverside.Markup.InteropServices/DocumentTypes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riverside.Markup.InteropServices
+{
+    /// <summary>
+    /// This enumeration defines the different types of documents that can be translated into Rosetta.
+    /// </summary>
+    public enum DocumentTypes
+    {
+        /// <summary>
+        /// HyperText Markup Language document.
+        /// </summary>
+        HyperText,
+
+        /// <summary>
+        /// Microsoft Word document.
+        /// </summary>
+        Word,
+
+        /// <summary>
+        /// Portable Document Format.
+        /// </summary>
+        PortableDocument,
+
+        /// <summary>
+        /// Rich Text Format document.
+        /// </summary>
+        RichText,
+
+        /// <summary>
+        /// Markdown document.
+        /// </summary>
+        Markdown,
+
+        /// <summary>
+        /// ReStructuredText document.
+        /// </summary>
+        ReStructuredText,
+    }
+}
